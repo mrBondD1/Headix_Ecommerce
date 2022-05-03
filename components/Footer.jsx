3eea53064481338165104aae3bc6ead0fbdef15b
mrBondD1 <hrxbond@gmail.com>
2022-05-03 16:15:44 +0600

    added state context

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,13 +4,13 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>2022 HEADIX HeadPhones All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
       </p>
     </div>
-  )
+  );
 }
 
 export default Footer
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import Link from 'next/link';
-// import { AiOutlineShopping } from 'react-icons/ai'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
 
-// import { Cart } from './';
-// import { useStateContext} from '../context/StateContext';
+import { Cart } from './';
+import { useStateContext} from '../context/StateContext';
 
-// const Navbar = () => {
-//   const { showCart, setShowCart, totalQuantities } = useStateContext();
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-//   return (
-//     <div className="navbar-container">
-//       <p className="logo">
-//         <Link href="/">JSM Headphones</Link>
-//       </p>
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">HEADIX HeadPhones</Link>
+      </p>
 
-//       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-//         <AiOutlineShopping />
-//         <span className="cart-item-qty">{totalQuantities}</span>
-//       </button>
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
 
-//       {showCart && <Cart />}
-//     </div>
-//   )
-// }
+      {showCart && <Cart />}
+    </div>
+  )
+}
 
-// export default Navbar
+export default Navbar
