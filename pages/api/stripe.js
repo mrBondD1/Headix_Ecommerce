8b29e83ab769a9e492bfc21248269e5acc139379
mrBondD1 <hrxbond@gmail.com>
2022-05-04 14:22:57 +0600

    stripe integration

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -6,36 +6,42 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const params = {
-        submit_type: 'pay',
-        mode: 'payment',
-        payment_method_types: ['card'],
-        billing_address_collection: 'auto',
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: "shr_1KvQ6JBxK1ESA83yqIqTbC74" },
+          { shipping_rate: "shr_1KvQ8rBxK1ESA83yvdcqYc4G" },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/xhlrj5x1/production/"
+            )
+            .replace("-webp", ".webp");
 
           return {
-            price_data: { 
-              currency: 'usd',
-              product_data: { 
+            price_data: {
+              currency: "usd",
+              product_data: {
                 name: item.name,
                 images: [newImage],
               },
               unit_amount: item.price * 100,
             },
             adjustable_quantity: {
-              enabled:true,
+              enabled: true,
               minimum: 1,
             },
-            quantity: item.quantity
-          }
+            quantity: item.quantity,
+          };
         }),
         success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/canceled`,
-      }
+      };
 
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
